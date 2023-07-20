761a92db21623986e76ad65475656b1ce4729c58
Bahare <itxbb09@gmail.com>
2023-07-20 21:25:33 +0100

    Projects.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
-// import Projects from "./components/Projects";
+import Projects from "./components/Projects";
 import ContactForm from "./components/ContactForm";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <main>
         <AboutMe />
-        {/*<Projects />*/}
+        <Projects />
         <ContactForm />
       </main>
       <footer>
