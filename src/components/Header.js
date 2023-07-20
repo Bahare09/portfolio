27d48520ff4bce008f0e69cbe69fab27b3496b3b
Bahare <itxbb09@gmail.com>
2023-07-20 22:19:45 +0100

    Header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header>
-      <h1>Your Name</h1>
-      <nav>{/* Add navigation links here */}</nav>
+      <h1>Bahare Bahreinian</h1>
+      <nav></nav>
     </header>
   );
 };
