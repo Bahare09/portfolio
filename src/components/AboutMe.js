86f68249307226f41a91efd15ec74501b2685dda
Bahare <108987748+Bahare09@users.noreply.github.com>
2023-07-25 23:33:55 +0100

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ import React from "react";
 const AboutMe = () => {
   return (
     <section id="about">
-      <h2>About Me</h2>
+      <h1>About Me</h1>
       <p>
         Hi! I'm Bahare, a passionate developer with a love for creating awesome
         websites and applications.
