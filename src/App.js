174b3e1fe48ee69a50bda8c746325849ef327062
Bahare <itxbb09@gmail.com>
2023-07-27 22:07:12 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <AboutMe />
         <Projects />
         <ContactForm />
+        <Footer />
       </main>
       <footer>
         <p>&copy; {new Date().getFullYear()} Bahare Bahreinian</p>
