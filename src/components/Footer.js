ebfe70e0c7b3191fd1f057cb7ac0ba7f88b9fd10
Bahare <itxbb09@gmail.com>
2023-07-27 22:04:43 +0100

    update footer.js and style.css

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+// Footer.js
+
+import React from "react";
+import "./Style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+        <div className="social-links">
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
