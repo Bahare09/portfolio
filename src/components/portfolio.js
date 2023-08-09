c679d0495bb95837da9969f6d01527540190dc6d
Bahare <itxbb09@gmail.com>
2023-08-09 20:22:48 +0100

    portfolio

---
+++ src/components/portfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const portfolio = () => {
+  return (
+    <header>
+      <h1>Bahare Bahreinian</h1>
+      <nav></nav>
+    </header>
+  );
+};
+
+export default portfolio;
