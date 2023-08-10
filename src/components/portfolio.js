51394472721feafc53aff1a5fda2401e7f357802
Bahare <itxbb09@gmail.com>
2023-08-10 21:48:44 +0100

    update portfolio.js

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,13 +1,6 @@
 import React from "react";
 import "./Header.css";
 
-const portfolio = () => {
-  return (
-    <header>
-      <h1>Bahare Bahreinian</h1>
-      <nav></nav>
-    </header>
-  );
-};
+const portfolio = () => {};
 
 export default portfolio;
